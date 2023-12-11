ddd9853b31a62ac3b2305db645950458afa0892b
ganger <malenko.2003@mail.ru>
2023-12-11 05:02:15 +0300

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/blyablyablya/',
   server: { port: 3000,
     proxy:{
       '/api': 'http://localhost:8080',
     }
+    
   },
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'src/index.html',
-    },
-  },
 })
